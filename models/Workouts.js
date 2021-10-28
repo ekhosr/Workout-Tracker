63f9e7f335a6b2f348eaa4c12a2f4c0c014ef191
Ehsan Khosravi <ekhosr@gmail.com>
2021-10-28 19:27:02 -0400

    module workout test

---
+++ models/Workouts.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+      },
+      duration: {
+        type: Number,
+      },
+      name: {
+        type: String,
+        trim: true,
+      },
+      weight: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
+  totalDuration: {
+    type: Number,
+  }
+});
+const Workout = mongoose.model("workouts", workoutSchema);
+module.exports = Workout;
